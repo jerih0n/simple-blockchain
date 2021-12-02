68fe941011977cf0699f6c54d9e73bfa16137299
PC <PC@DESKTOP-MA4RSJC>
2021-12-02 11:42:52 +0200

    initial commit

---
+++ Mono/Node/SimpleBlockchain/Blockchain.Node.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blockchain.Node.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
