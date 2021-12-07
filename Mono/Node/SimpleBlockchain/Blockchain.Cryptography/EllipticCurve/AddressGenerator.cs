a7bc498468553aec4442bc6dc9fb2f4020b4642d
PC <PC@DESKTOP-MA4RSJC>
2021-12-07 23:57:24 +0200

    changing the way addresses are generated

--- Mono/Node/SimpleBlockchain/Blockchain.Cryptography/EllipticCurve/AddressGenerator.cs
+++ Mono/Node/SimpleBlockchain/Blockchain.Cryptography/EllipticCurve/AddressGenerator.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace Blockchain.Cryptography.EllipticCurve
 {
     public static class AddressGenerator
     {
-        private const string ADDRESS_PREFIX = "bchain_0x_{0}_{1}";
+        private const string ADDRESS_PREFIX = "mychain_{0}_{1}";
 
         public static string GenerateFirstAddress(byte[] publicKey)
         {
-            var rowAddress = SHA256.Create().ComputeHash(publicKey);
-            var base64 = Convert.ToBase64String(rowAddress);
-            return string.Format(ADDRESS_PREFIX, 0, base64);
+            var newAddressSha = SHA256.Create().ComputeHash(publicKey);
+            return CreateAddress(newAddressSha, 0);
         }
 
         public static string GenerateNextAddress(string address)
@@ -21,22 +18,28 @@ namespace Blockchain.Cryptography.EllipticCurve
             try
             {
                 var addressSections = address.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
-                if (addressSections[0].ToLower() == "bchain" && addressSections[1] == "0x")
+                if(addressSections[0].ToLower() == "mychain_")
                 {
-                    var addressNumber = int.Parse(addressSections[2]);
-                    addressNumber += 1;
-                    var base64Hash = addressSections[3];
-                    var bytes = Convert.FromBase64String(base64Hash);
-                    var newAddress = SHA256.Create().ComputeHash(bytes);
-                    var newAddressAsBase64String = Convert.ToBase64String(newAddress);
-                    return string.Format(ADDRESS_PREFIX, addressNumber, newAddressAsBase64String);
+                    throw new Exception("Invalid Address");
                 }
+
+                var addressNumber = int.Parse(addressSections[1]);
+                addressNumber += 1;
+                var base64Hash = addressSections[2];
+                var bytes = Convert.FromBase64String(base64Hash);
+                var newAddressSha = SHA256.Create().ComputeHash(bytes);
+                return CreateAddress(newAddressSha, addressNumber);
+                
             }catch
             {
                 throw new Exception("Invalid input");
             }
-           
-            throw new NotImplementedException();
+        }
+
+        private static string CreateAddress(byte[] sha256, int addressNumber)
+        {
+            var sha256AsString = Convert.ToBase64String(sha256).ToLower();
+            return string.Format(ADDRESS_PREFIX, addressNumber, sha256AsString);
         }
     }
 }
